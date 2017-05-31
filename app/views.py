4dbc5d69ff1acfa9a07c8ac0845805624a2dc88c
root <root@localhost.localdomain>
2017-05-31 20:53:03 +0000

    ansible additions

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+import os
 from flask import render_template
 from app import app
 from .forms import FlaskForm
+from ansible.playbook import Playbook
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -10,6 +12,8 @@ def index():
 	  vmname = form.vmname.data
 	  vmsize = form.vmsize.data
 	  vmos = form.vmos.data
+	  pb = Playbook(playbook='/common/conf/ansible/playbooks/test.yaml')
+	  pb.run()
 	  return render_template('result.html',title='Build Result', vmname=vmname, vmsize=vmsize, vmos=vmos)
 	return render_template('request.html',
 				title='Build VM',
